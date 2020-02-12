8115694e65e25325a858bf02eaec2140481339a9
jadibrahim99 <jadintx@gmail.com>
2020-02-12 17:36:13 -0600

    Variables set and functions created, no for loops or if statements included in the current code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,32 @@ var guesses = 9;
 var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
-
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(computerGuess);
 
 
+function updateGuessesLeft() {
+    // Here we are grabbing the HTML element and setting it equal to the guessesLeft
+    document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+    // Here we are updating the letters that you are still able to guess
+function updateLetterToGuess() {
+    this.letterToGuess = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+};
 
 
+function updateGuesses() {
+    // Here we take the guesses the user has tried and then display it in the HTML
+    document.querySelector('#guess').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
 
-
+// Here we reset the variables for a new game using a function
+var reset = function() {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuesses();
+}
+updateLetterToGuess();
+updateGuessesLeft();

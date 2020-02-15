bb5e673dc7a9664e1701c2ec81507790cb7e936e
jadibrahim99 <jadintx@gmail.com>
2020-02-15 15:52:33 -0600

    Javascript code nearly finished, weird bug where letters guessed do not clear. No Css added at this point

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,27 +13,42 @@ var randomLetter = function () {
 };
 
 // using a function to grab the html that represents "guesses" and concatinating it with guesses the user has made so far
-var guesssoFar = function () {
+var guessed = function () {
     document.getElementById("guesses").innerHTML = "Guesses so far: " + guessesSoFar.join(",");
 };
 // same basic function as above, but this function is changing the html of the guesses that the user has left
 var guessesLeft = function () {
     document.getElementById("left").innerHTML = "Guesses Left: " + left;
 };
-
+// this variable is set equal to a function which resets the stats of the game i.e guessed letters, wins, losses etc.
 var reset = function () {
     guessedLetters = [];
     left = 9;
     randomLetter();
     guessesLeft();
-    guesssoFar();
+    guessed();
 }
-
+// everytime the user types a key one of their guesses left is subtracted by one
 document.onkeyup = function (event) {
     var userGuess = event.key;
     left--;
     guessesSoFar.push(userGuess);
-    guesssoFar();
+    guessed();
     guessesLeft();
-   
+
+
+    
+    // when the user guesses correctly the wins are tallied under the wins html
+    if (left > 0) {
+        if (userGuess == computerGuess) {
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins:" + wins;
+            reset();
+        }
+        //when the user runs out of guesses the losses are tallied and the losses html is changed
+    } else if (left == 0) {
+        losses++;
+        document.getElementById("losses").innerHTML = "Losses:" + losses;
+        reset();
+    }
 };
